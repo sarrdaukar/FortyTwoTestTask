b8ed5c8357907d5fdc63a715dc1a702c4db76980
Yaroslav Cheb <sarrdaukar@gmail.com>
2020-01-31 18:06:39 +0200

    Ticket #1 add test for Profile model

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -24,7 +24,44 @@ class TestMainView(TestCase):
         self.assertEqual(profile.last_name, "Cheb")
         self.assertEqual(profile.birthdate, date(1992, 11, 6))
         self.assertEqual(profile.bio, "My short biography")
-        self.assertEqual(profile.email, 'mail@mail.com')
-        self.assertEqual(profile.jabber, 'jabber@jabber.com')
-        self.assertEqual(profile.skype, 'skype_nick')
-        self.assertEqual(profile.other_contacts, 'Other contacts')
+        self.assertEqual(profile.email, "mail@mail.com")
+        self.assertEqual(profile.jabber, "jabber@jabber.com")
+        self.assertEqual(profile.skype, "skype_nick")
+        self.assertEqual(profile.other_contacts, "Other contacts")
+
+
+class TestProfileModel(TestCase):
+
+    def setUp(self):
+        profile = Profile(
+            name="Yaroslav", last_name="Cheb", birthdate=date(1992, 11, 6),
+            bio="My short biography", email="mail@mail.com",
+            jabber="jabber@jabber.com", skype="skype_nick",
+            other_contacts="Other contacts"
+        )
+        profile.save()
+        self.profile = profile
+
+    def test_profile_load(self):
+        "Test Profile load method"
+        profile = Profile.load()
+        self.assertEqual(profile.id, Profile.INSTANCE_ID)
+        self.assertEqual(profile.name, self.profile.name)
+        self.assertEqual(profile.last_name, self.profile.last_name)
+        self.assertEqual(profile.bio, self.profile.bio)
+        self.assertEqual(profile.email, self.profile.email)
+        self.assertEqual(profile.jabber, self.profile.jabber)
+        self.assertEqual(profile.skype, self.profile.skype)
+        self.assertEqual(profile.other_contacts, self.profile.other_contacts)
+
+    def test_profile_model_has_only_one_object(self):
+        "Test if Profile always has one db instance"
+        profile_2 = Profile(
+            name="John", last_name="Smith", birthdate=date(2000, 2, 22),
+            bio="Bio of John Smith", email="johnsmith@mail.com",
+            jabber="johnsmith@jabber.com", skype="johnsmith_skype",
+            other_contacts="Contacts"
+        )
+        profile_2.save()
+        # first profile object created in setUp
+        self.assertEqual(Profile.objects.count(), 1)
