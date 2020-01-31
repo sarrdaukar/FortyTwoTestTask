73528ec5e987d81db1d5bcf7cbcccb26cdce7aca
Yaroslav Cheb <sarrdaukar@gmail.com>
2020-01-31 16:47:56 +0200

    Ticket #1: add test for main view with hardcoded data

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,30 @@
-from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from django.test import Client, TestCase
+from django.core.urlresolvers import reverse
 
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 4)
+class TestMainView(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_main_view(self):
+        """
+        test main view returning correct statuscode,
+        using right template and having all necessary data in context
+        """
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "hello/index.html")
+        self.assertIn("profile", response.context)
+
+        profile = response.context["profile"]
+        self.assertEqual(profile.name, "Yaroslav")
+        self.assertEqual(profile.last_name, "Cheb")
+        self.assertEqual(profile.birthdate, date(1992, 11, 6))
+        self.assertEqual(profile.bio, "My short biography")
+        self.assertEqual(profile.email, 'mail@mail.com')
+        self.assertEqual(profile.jabber, 'jabber@jabber.com')
+        self.assertEqual(profile.skype, 'skype_nick')
+        self.assertEqual(profile.other_contacts, 'Other contacts')
