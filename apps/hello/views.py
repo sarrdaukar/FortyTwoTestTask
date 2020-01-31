2e7ab32207735dba0207395076b2c3156fd19c85
Yaroslav Cheb <sarrdaukar@gmail.com>
2020-01-31 18:54:24 +0200

    Ticket #1: Change main view to use Profile

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,20 +1,8 @@
-from collections import namedtuple
-from datetime import date
-
 from django.shortcuts import render
 
-Profile = namedtuple(
-    'Profile',
-    [
-        'name', 'last_name', 'birthdate', 'bio', 'email', 'jabber', 'skype',
-        'other_contacts'
-    ]
-)
+from apps.hello.models import Profile
 
 
 def main(request):
-    profile = Profile(
-        'Yaroslav', 'Cheb', date(1992, 11, 6), 'My short biography',
-        'mail@mail.com', 'jabber@jabber.com', 'skype_nick', 'Other contacts'
-    )
+    profile = Profile.load()
     return render(request, 'hello/index.html', locals())
